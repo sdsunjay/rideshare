24b871429bb692e2adf666713396bab5fcef724b
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-02-28 20:22:50 -0800

    config file

---
+++ gather/configuration.php
@@ -0,0 +1,18 @@
+<?php
+define('REDIRECT_LOGIN_URL', 'https://www.cprideshare.com/github/rideshare/php/fb-callback.php');
+define('REDIRECT_MEMBERS_URL', 'Location: https://www.cprideshare.com/github/rideshare/php/getPosts.php');
+define('SESSION_ID_NUM','DgBCzrVWbbADgNBWWpN');
+
+define('APP_ID','');
+define('APP_SECRET','');
+define('GROUP_ID','250502971675365');
+define('MAPS_API_KEY','');
+define('DATABASE_NAME','new_projects');
+define('OLD_DATABASE_NAME','projects');
+define('POSTS_TABLE','POSTS');
+define('CLEAN_POSTS_TABLE','clean_posts');
+define('USERNAME','root');
+define('PASSWORD','');      
+define('HOST','localhost');
+
+?>
